bba12d358986a0a5dd7d1da3171f928084d0dd2a
Alvaro <adigod00@estudiantes.unileon.es>
2016-04-08 10:21:15 +0200

    Dia 3

--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -2,8 +2,10 @@ package es.unileon.prg1.date;
 
 public class MainDate{
 
-	public static void main(String args[]){
-
+	public static void main(String []args) throws DateException{
+		Date today;
+		today = new Date(5,2,2016);
+		System.out.println(today);
 	}
 
 }	
